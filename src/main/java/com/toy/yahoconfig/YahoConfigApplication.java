43f131c7045d371b910bfe19ac1cac072c8f560c
JongChan <kjc3590@songareeit.com>
2021-01-06 17:28:39 +0900

    init

---
+++ src/main/java/com/toy/yahoconfig/YahoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.toy.yahoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YahoConfigApplication.class, args);
+    }
+
+}
